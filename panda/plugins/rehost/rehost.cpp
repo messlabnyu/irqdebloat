df34e0c7ee816e8c5a12a3574757266195a9b14d
Nick Gregory <computerfreak97@gmail.com>
2017-04-07 04:57:31 +0000

    Few organizational changes. Added ability to skip functions (just add skip_func as a hook). Made logging/debug printing nicer

--- panda/plugins/rehost/rehost.cpp
+++ panda/plugins/rehost/rehost.cpp
@@ -14,6 +14,8 @@ PANDAENDCOMMENT */
 #include <string>
 #include <unordered_map>
 #include <map>
+#include <vector>
+#include <algorithm>
 #include <time.h>
 #include <fstream>
 #include <sstream>
@@ -27,43 +29,164 @@ void uninit_plugin(void *);
 
 }
 
+
+// State tracking
 device_t last_device = UNKNOWN_DEVICE;
 clock_t last_device_time = 0;
 
-void set_last_device(device_t type)
+
+/*
+ * Guest function hooks
+ */
+
+bool set_last_device(device_t type)
 {
     last_device = type;
     last_device_time = clock();
+
+    return 0;
 }
 
-void print_hook(CPUState *cpu)
+bool print_hook(CPUState *cpu, TranslationBlock *tb)
 {
     uint8_t buf[1024];
     CPUArchState *env = (CPUArchState*)cpu->env_ptr;
-    target_ulong str_ptr = env->regs[0];
+    target_ulong str_ptr = env->regs[0]; // TODO: Architecture neutral
 
     panda_virtual_memory_read(cpu, str_ptr, buf, sizeof(buf));
 
     printf("%s", buf);
+    
+    return 0;
 }
 
-void poweroff_hook(CPUState *cpu)
+bool poweroff_hook(CPUState *cpu, TranslationBlock *tb)
 {
-    printf("Machine should restart or shutdown\n");
+    DEBUG("Machine should restart or shutdown now.");
+    
+    return 0;
 }
 
-std::unordered_map<target_ulong, hook_func_t> hooks;
-std::map<std::string, target_ulong> kallsyms;
+/*
+ * List of addresses that have already been patched by the skip function.
+ * We need to keep this to prevent an endless loop of exec->re-translate->exec->...
+ * We can't write all of these at load time because things may not be loaded/decompressed yet.
+ * We only care about things in kernel-land right now so there's no chance we'll have a
+ *  virtual address overlap issue.
+ */
+std::vector<target_ulong> patched_funcs;
 
+// mov r0, #0; bx lr
+// TODO: architecture-independent
+uint8_t patch_asm[] = {0x00, 0x00, 0xa0, 0xe3, 0x1e, 0xff, 0x2f, 0xe1};
+
+bool skip_func(CPUState *cpu, TranslationBlock *tb)
+{
+    target_ulong addr = tb->pc;
+
+    if (std::find(patched_funcs.begin(), patched_funcs.end(), addr) == patched_funcs.end()) {
+        DEBUG("Patching function at 0x" TARGET_FMT_lx, addr);
+        panda_virtual_memory_write(cpu, addr, patch_asm, sizeof(patch_asm));
+        patched_funcs.push_back(addr);
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+/*
+ * Plugin-wide maps
+ */
+
+std::unordered_map<target_ulong, std::vector<hook_func_t>> hooks;
+std::map<std::string, target_ulong> kallsyms;
 std::map<std::string, hook_func_t> readable_hooks = {
     {"printk", print_hook},
     {"printascii", print_hook},
-    {"init_IRQ", [](CPUState *cpu) { set_last_device(INTERRUPT_CONTROLLER_DIST); }},
-    {"gic_cpu_init", [](CPUState *cpu) { set_last_device(INTERRUPT_CONTROLLER_CPU); }},
-    {"uart_register_driver", [](CPUState *cpu) { set_last_device(UART_DEVICE); }},
+    {"init_IRQ", [](CPUState *cpu, TranslationBlock *tb)
+        {
+            return set_last_device(INTERRUPT_CONTROLLER_DIST);
+        }
+    },
+    {"gic_cpu_init", [](CPUState *cpu, TranslationBlock *tb)
+        {
+            return set_last_device(INTERRUPT_CONTROLLER_CPU);
+        }
+    },
+    {"uart_register_driver", [](CPUState *cpu, TranslationBlock *tb)
+        {
+            return set_last_device(UART_DEVICE);
+        }
+    },
     {"die", poweroff_hook},
     {"machine_restart", poweroff_hook},
 };
+
+
+/*
+ * PANDA callback functions
+ */
+
+bool before_block_exec_invalidate_opt(CPUState *cpu, TranslationBlock *tb)
+{
+    int ret = 0;
+
+    auto func_hooks = hooks.find(tb->pc);
+    if (func_hooks != hooks.end()) {
+        for (auto &hook : func_hooks->second) {
+            ret |= (*hook)(cpu, tb);
+        }
+    }
+
+    if (ret)
+        DEBUG("Invalidating the translation block at 0x" TARGET_FMT_lx, tb->pc);
+
+    return ret;
+}
+
+int check_unassigned_mem_r(CPUState *cpu, target_ulong pc, target_ulong addr, target_ulong size)
+{
+    MemoryRegion *subregion;
+    
+    QTAILQ_FOREACH(subregion, &cpu->memory->subregions, subregions_link) {
+        if (addr >= subregion->addr && addr < subregion->addr + subregion->size) {
+            // addr is in a defined memory region, so just let QEMU process it as normal
+            return 0;
+        }
+    }
+
+    // This memory read is not in any existing MemoryRegion, so report it to the master
+
+    DEBUG("Unassigned read at 0x" TARGET_FMT_lx, addr);
+    DEBUG("Current last device: %u set at time %lu", last_device, last_device_time);
+    
+    return 0;
+}
+
+int check_unassigned_mem_w(CPUState *cpu, target_ulong pc, target_ulong addr,
+                           target_ulong size, void *buf)
+{
+    MemoryRegion *subregion;
+    
+    QTAILQ_FOREACH(subregion, &cpu->memory->subregions, subregions_link) {
+        if (subregion->addr <= addr && addr < subregion->addr + subregion->size) {
+            return 0;
+        }
+    }
+
+    DEBUG("Unassigned write at 0x" TARGET_FMT_lx, addr);
+    DEBUG("Current last device: %u set at time %lu", last_device, last_device_time);
+
+    last_device = UNKNOWN_DEVICE;
+    
+    return 0;
+}
+
+
+/*
+ * Plugin initialization
+ */
 
 void parse_sym_file(const char *sym_file)
 {
@@ -90,57 +213,13 @@ void parse_sym_file(const char *sym_file)
     for (auto hook = readable_hooks.begin(); hook != readable_hooks.end(); hook++) {
         auto symbol = kallsyms.find(hook->first);
         if (symbol != kallsyms.end()) {
-            hooks[symbol->second] = hook->second;
+            auto sym_addr = symbol->second;
+            auto hook_func = hook->second;
+            hooks[sym_addr].push_back(hook_func);
         } else {
             std::cout << "WARNING: Function " << hook->first << " not in kallsyms" << std::endl;
         }
     }
-}
-
-int before_block_exec(CPUState *cpu, TranslationBlock *tb)
-{
-    auto hook = hooks.find(tb->pc);
-    if (hook != hooks.end()) {
-        (*hook->second)(cpu);
-    }
-
-    return 0;
-}
-
-int check_unassigned_mem_r(CPUState *cpu, target_ulong pc, target_ulong addr, target_ulong size)
-{
-    MemoryRegion *subregion;
-    
-    QTAILQ_FOREACH(subregion, &cpu->memory->subregions, subregions_link) {
-        if (addr >= subregion->addr && addr < subregion->addr + subregion->size) {
-            // addr is in a defined memory region, so just let QEMU process it as normal
-            return 0;
-        }
-    }
-
-    // This memory read is not in any existing MemoryRegion, so report it to the master
-
-    printf("rehost: Unassigned read at 0x" TARGET_FMT_lx ". ", addr);
-    printf("Current last device: %u\n", last_device);
-    
-    return 0;
-}
-
-int check_unassigned_mem_w(CPUState *cpu, target_ulong pc, target_ulong addr,
-                           target_ulong size, void *buf)
-{
-    MemoryRegion *subregion;
-    
-    QTAILQ_FOREACH(subregion, &cpu->memory->subregions, subregions_link) {
-        if (subregion->addr <= addr && addr < subregion->addr + subregion->size) {
-            return 0;
-        }
-    }
-
-    printf("rehost: Unassigned write at 0x" TARGET_FMT_lx ". ", addr);
-    printf("Current last device: %u\n", last_device);
-    
-    return 0;
 }
 
 bool init_plugin(void *self)
@@ -149,9 +228,11 @@ bool init_plugin(void *self)
     panda_arg_list *args;
     const char *sym_file;
 
-    cb.before_block_exec = before_block_exec;
-    panda_register_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC, cb);
-    
+    // May not be necessary but to afraid that not having this will silently break stuff
+    panda_disable_tb_chaining();
+    cb.before_block_exec_invalidate_opt = before_block_exec_invalidate_opt;
+    panda_register_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC_INVALIDATE_OPT, cb);
+   
     panda_enable_memcb();
     cb.virt_mem_before_read = check_unassigned_mem_r;
     panda_register_callback(self, PANDA_CB_PHYS_MEM_BEFORE_READ, cb);
@@ -170,4 +251,5 @@ bool init_plugin(void *self)
 
 void uninit_plugin(void *self)
 {
+    // TODO: Dump new things to master
 }
--- panda/plugins/rehost/rehost.h
+++ panda/plugins/rehost/rehost.h
@@ -12,6 +12,15 @@ enum device_t {
     NAND_CONTROLLER_DEVICE
 };
 
-typedef void (*hook_func_t)(CPUState *);
+typedef bool (*hook_func_t)(CPUState *, TranslationBlock *);
+
+#define DEBUG_REHOST_PLUGIN
+
+#ifdef DEBUG_REHOST_PLUGIN
+#define DEBUG(fmt, ...) \
+    do { fprintf(stderr, "panda_rehost: " fmt "\n", ## __VA_ARGS__); } while (0)
+#else
+#define DEBUG(fmt, ...) do {} while(0)
+#endif
 
 #endif
